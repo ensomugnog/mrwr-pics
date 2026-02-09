dc1d19e9822e2010d9bb9a5ed9d1b0c3323ff5f0
Enso Mugno Galvis <ensomugnog@gmail.com>
2026-02-09 13:36:34 +1000

    10 - Data Fetching Cleanup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import searchImages from './api';
-
-searchImages();
-
+ 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 

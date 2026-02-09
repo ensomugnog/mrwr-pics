8a4114f503ab94ae272944dcb406b1ac080cf392
Enso Mugno Galvis <ensomugnog@gmail.com>
2026-02-09 10:02:17 +1000

    08 - Making an HTTP Request

---
+++ src/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const searchImages = async () => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+        headers: {
+            Authorization: 'Client-ID 1-L_PQrOtJ3jV_7S43lt2XfCyE6GJhTaFiQOx8sbAxU'
+        },
+        params: {
+            query: 'cars'
+        }
+    });
+
+    console.log(response);
+
+    return response;
+};
+
+export default searchImages;

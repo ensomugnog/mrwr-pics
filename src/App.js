097973d50caa4506c2eb0f78da4f2621c071dba6
Enso Mugno Galvis <ensomugnog@gmail.com>
2026-02-10 05:23:54 +1000

    13 - Implementing Child to Parent Communication

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import SearchBar from './components/SearchBar';
+
+function App() {
+
+    const handleSubmit = (term) => {
+        console.log('Do a search with', term);
+    };
+
+    return (
+        <SearchBar onSubmit={handleSubmit} />
+    );
+};
+
+export default App;
